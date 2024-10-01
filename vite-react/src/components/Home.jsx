83d8a198d146ec6d372e72cdc7bff925ee9204cc
Kathy Sagredo <sagredo.kathy@gmail.com>
2024-10-01 16:32:46 -0300

    Instalar React Router y estructura básica

---
+++ vite-react/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+
+function Home(){
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/Componentes">Componentes</Link></li>
+                <li><Link to="/Props">Props</Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Home
